c1f568d3b4bac940f7e9654f7b7b075a65430de9
Victor Mello <victor.mello@al.infnet.edu.br>
2022-05-03 23:49:56 -0300

    implementadoss componentes que representam os tipos de feedback (problema, ideia ou outro) - Parte 2

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -4,6 +4,7 @@ import ideaImageUrl from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -42,21 +43,17 @@ export type FeedbackType = keyof typeof feedbackTypes
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+  function handleRestartFeedback(){
+    setFeedbackType(null)
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-
-      {/* Cabeçalho */}
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-
       {/* Conteúdo */}
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
-      ) : (
-        <p>Hello World</p>
-        //Implementar o componente FeedbackContentStep
+      ) : (        
+        <FeedbackContentStep feedbackType={feedbackType} onFeedbackRestartRequested={handleRestartFeedback}/>
       )}
 
       {/* Rodapé */}
